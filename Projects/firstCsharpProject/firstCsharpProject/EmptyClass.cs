f47ebe8ea63f78cb95cad624e55659e9d0d496c4
Bharath.Nadukatla <Bharath.Nadukatla@quovant.com>
2018-05-23 21:36:49 +0530

    Initial Commit. Coded methods and test that runs successfully.

---
+++ Projects/firstCsharpProject/firstCsharpProject/EmptyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace firstCsharpProject
+{
+    public class StringManipulation
+    {
+        //static void Main(string[] args)
+        //{
+        //    string x = "Bharath Nadukatla";
+        //    Console.WriteLine("x.ToUpper:" + x.ToUpper());
+        //    Console.WriteLine("x.Substring(4):" + x.Substring(4));
+        //    Console.WriteLine("x.Substring(4,8):" + x.Substring(4, 8));
+        //    Console.WriteLine("x.IndexOf('a'):" + x.IndexOf('a'));
+        //    Console.WriteLine("x[3]:" + x[3]);
+        //    //Console.WriteLine("Splitting X:" + x.Split());
+
+        //    string[] arr = { "one", "two", "three" };
+        //    foreach( string num in arr ){
+        //        Console.WriteLine("Current number: " + num);
+        //    }
+
+        //}
+    
+    }
+}
---
+++ Projects/firstCsharpProject/firstCsharpProject/lib/FirstTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
+
+namespace firstCsharpProject.lib
+{
+    public class FirstTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            //driver = new ChromeDriver("/Users/admin/Projects/firstCsharpProject/firstCsharpProject/drivers");
+            driver = new SafariDriver();
+            driver.Manage().Window.Maximize();
+        }
+ 
+        [Test]
+        public void Test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
